b520f5bf2241d3f8ba8db3fc0fcec5cb09c8348a
Sergey Sokov <62668194+Sokovtlt@users.noreply.github.com>
2025-06-07 12:41:24 +0200

    Update urls.py
    rewrite path

--- account/urls.py
+++ account/urls.py
@@ -19,21 +19,5 @@ urlpatterns = [
     path('password-reset/complete/', views.UserPasswordResetCompleteView.as_view(), name='password_reset_complete'),
     # # customer
     path('customer/<slug:slug>/', views.DetailCustomerView.as_view(), name='detail_customer_page'),
-    # path('customer/edit/<slug:slug>/', views.UpdateCustomerView.as_view(), name='update_customer_page'),
     path('your-dashboard/', views.DashboardCustomerView.as_view(), name='dashboard_customer_page'),
-    # # station
-    # path('station/<slug:slug>/', views.DetailStationView.as_view(), name='detail_station_page'),
-    # path('station/edit/<slug:slug>/', views.UpdateStationView.as_view(), name='update_station_page'),
-    # path('dashboard/', views.DashboardStationView.as_view(), name='dashboard_station_page'),
-    # # support customer
-    # path('customer-support/', views.SupportCsView.as_view(), name='customer_sp_page'),
-    # path('customer-ticket/<int:pk>/', views.DetailSupportCsView.as_view(), name='detail_support_cs_page'),
-    # # support station
-    # path('station-support/', views.SupportStView.as_view(), name='station_sp_page'),
-    # path('station-ticket/<int:pk>/', views.DetailSupportStView.as_view(), name='detail_support_st_page'),
-    # # admin
-    # path('admin-checking-offers-for-date/', views.CheckingOffersView.as_view(), name='admin_checking_offers_page'),
-    # path('test/', views.TestView.as_view(), name='test'),
-    # path('owner-dashboard/', views.OwnerDashboardView.as_view(), name='dashboard_owner_page'),
-    # path('support-dashboard/', views.SupportDashboardView.as_view(), name='dashboard_support_page'),
 ]
